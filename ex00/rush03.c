519260fcc17b217ef4f5140da28492fb6be762e2
Min Thu Khaing <mikhaing@student.42singapore.sg>
2025-07-26 14:37:52 +0800

    rush03 done

--- ex00/rush03.c
+++ ex00/rush03.c
@@ -6,11 +6,43 @@
 /*   By: mikhaing <0x@bontal.net>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 10:40:52 by mikhaing          #+#    #+#             */
-/*   Updated: 2025/07/26 11:48:10 by mikhaing         ###   ########.fr       */
+/*   Updated: 2025/07/26 14:37:33 by mikhaing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
+void	ft_put_line(int width, char left, char mid, char right)
+{
+	int	i;
+
+	if (width <= 0)
+		return ;
+	ft_putchar(left);
+	i = 1;
+	while (i < width - 1)
+	{
+		ft_putchar(mid);
+		++i;
+	}
+	if (width > 1)
+		ft_putchar(right);
+}
+
 void	rush(int x, int y)
-{}
+{
+	int	row;
+
+	if (x <= 0 || y <= 0)
+		return ;
+	row = 0;
+	while (row < y)
+	{
+		if (row == 0 || row == y - 1)
+			ft_put_line(x, 'A', 'B', 'C');
+		else
+			ft_put_line(x, 'B', ' ', 'B');
+		ft_putchar('\n');
+		++row;
+	}
+}
