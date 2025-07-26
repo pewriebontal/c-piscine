2185d84f78f0e1ff3f9b5d78a2b05f98f7c1b11b
Zeon Hee Chai Choo <zchoo@student.42singapore.sg>
2025-07-26 16:00:02 +0800

    update rush04 base on rush03's logic

--- ex00/rush04.c
+++ ex00/rush04.c
@@ -3,74 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   rush04.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mikhaing <0x@bontal.net>                   +#+  +:+       +#+        */
+/*   By: zchoo <zchoo@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 10:40:54 by zchoo             #+#    #+#             */
-/*   Updated: 2025/07/26 15:21:47 by mikhaing         ###   ########.fr       */
+/*   Updated: 2025/07/26 15:51:19 by zchoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-int	is_wall(int x, int y, int x_max, int y_max)
+void	ft_put_line(int width, char left, char mid, char right)
 {
-	return (x == 0 || x == x_max || y == 0 || y == y_max);
-}
+	int	i;
 
-int	is_corner(int x, int y, int x_max, int y_max)
-{
-	int	rtn;
-
-	rtn = 0;
-	if ((x == 0 && y == 0) || (x == 0 && y == y_max))
+	if (width <= 0)
+		return ;
+	ft_putchar(left);
+	i = 1;
+	while (i < width - 1)
 	{
-		rtn = 1;
+		ft_putchar(mid);
+		++i;
 	}
-	else if ((x == x_max && y == 0) || (x == x_max && y == y_max))
-	{
-		rtn = 1;
-	}
-	return (rtn);
-}
-
-void	print(int x, int y, int x_max, int y_max)
-{
-	if (is_corner(x, y, x_max, y_max))
-	{
-		if ((x == 0 && y == y_max && y != 0))
-			ft_putchar('C');
-		else if (x == x_max && y == 0 && x != 0)
-			ft_putchar('C');
-		else if ((x == 0 && y == 0) || (x == x_max && y == y_max))
-			ft_putchar('A');
-		else
-			ft_putchar('C');
-	}
-	else if (is_wall(x, y, x_max, y_max))
-	{
-		ft_putchar('B');
-	}
-	else
-	{
-		ft_putchar(' ');
-	}
+	if (width > 1)
+		ft_putchar(right);
 }
 
 void	rush(int x, int y)
 {
-	int	_y;
-	int	_x;
+	int	row;
 
-	_y = 0;
-	while (_y < y)
+	if (x <= 0 || y <= 0)
+		return ;
+	row = 0;
+	while (row < y)
 	{
-		_x = 0;
-		while (_x < x)
-		{
-			print(_y, _x, y - 1, x - 1);
-			_x++;
-		}
-		ft_putchar(10);
-		_y++;
+		if (row == 0)
+			ft_put_line(x, 'A', 'B', 'C');
+		else if (row == y - 1)
+			ft_put_line(x, 'C', 'B', 'A');
+		else
+			ft_put_line(x, 'B', ' ', 'B');
+		ft_putchar('\n');
+		++row;
 	}
 }
