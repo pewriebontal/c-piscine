ce2bf23fa05f6a61bd17ee4ca167a6a459f1231d
aisyayahaya <aiyahaya@student.42singapore.sg>
2025-08-02 17:14:02 +0800

    get clues

--- ex00/rush01.c
+++ ex00/rush01.c
@@ -6,12 +6,13 @@
 /*   By: aiyahaya <aiyahaya@student.42singapore.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 14:11:03 by aiyahaya          #+#    #+#             */
-/*   Updated: 2025/08/02 15:41:37 by aiyahaya         ###   ########.fr       */
+/*   Updated: 2025/08/02 17:09:32 by aiyahaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 //use stdlib.h for malloc & free
 
@@ -26,19 +27,45 @@ example:
 ./rush01 "4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 2"
 */
 
+// reminders: cannot def > 5 functions in one file
+
+char	*create_array(int size)
+{
+	char	*arr;
+	int		i;
+
+	arr = malloc(size * sizeof(int));
+	i = 0;
+	if (!arr)
+		return (NULL);
+	while (i < size)
+	{
+		arr[i] = i * 2;
+		i++;
+	}
+	return (arr);
+}
+
 // ASCII index for digits: 48-57
 int	ft_strlen(char *c)
 {
-	int	i;
-	int	count;
+	int		i;
+	int		count;
+	char	*result;
 
 	i = 0;
+	count = 0;
+	result = c;
 	while (c[i])
 	{
 		if (c[i] >= 48 && c[i] <= 57)
+		{
+			result[count] = c[i];
 			count++;
+		}
 		i++;
 	}
+	result[count] = '\0';
 	return (count);
 }
 
@@ -59,13 +86,55 @@ int	ft_gridsize(int x)
 }
 
 // function to return col/row arrays
-char	*ft_getarr(char *arr, char opt, int gridsize);
+// return col arrays when col = 1
+// return row arrays when col = 0
+char	*ft_getarr(char *arr, int col, int grid)
+{
+	char	*clues;
+	int		i;
 
-//
+	i = 0;
+	clues = create_array(grid);
+	while (i < grid * 2)
+	{
+		if (col == 1)
+			clues[i] = arr[i];
+		else
+			clues[i] = arr[(grid * 2) + i];
+		i++;
+	}
+	clues[i] = '\0';
+	return (clues);
+}
+
 int	main(int argc, char *argv[])
 {
+	int		size;
+	int		grid;
+	char	*clues;
+	char	*col_clues;
+	char	*row_clues;
+
 	if (argc == 2)
 	{
+		clues = argv[1];
+		size = ft_strlen(clues);
+		grid = ft_gridsize(size);
+		if (grid >= 4)
+		{
+			printf("Solving %d values in a %dx%d grid.\n", size, grid, grid);
+			printf("clues: %s\n", clues);
+			col_clues = ft_getarr(clues, 1, grid);
+			row_clues = ft_getarr(clues, 0, grid);
+			printf("cols:  %s\nrows:  %s\n", col_clues, row_clues);
+			free(col_clues);
+			free(row_clues);
+		}
+		else
+		{
+			write(1, "Error\n", 6);
+			return (1);
+		}
 		return (0);
 	}
 	else
